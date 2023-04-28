2ebee056c4678315fa3a3b02e50713275b28c1cb
lobinhodev <contatolobinhodev@gmail.com>
2023-04-27 21:24:57 -0300

    init: commit initial

---
+++ variables_dotnet-7/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numberInt = 0;
+int maxNumberInt = int.MaxValue;
+int minNumberInt = int.MinValue;
+
+long numberLong = 053264534265;
+long maxNumberLong = long.MaxValue;
+long minNumberLong = long.MinValue;
+
+decimal numberDecimal = 313.2m;
+decimal maxNumberDecimal = decimal.MaxValue;
+decimal minNumberDecimal = decimal.MinValue;
+
+double numberDouble = 313.2d;
+double maxNumberDouble = double.MaxValue;
+double minNumberDouble = double.MinValue;
+double minNumbderDouble = double.MinValue;
+
+bool conditionTrue = true;
+bool conditionFalse = false;
+
+string name = "Daniel";
+char letter = 'L';
+
+DateTime entryCompany = new DateTime(2023, 02, 02);
+TimeSpan timeCompany = (DateTime.Now) - entryCompany;
+
+Console.WriteLine(timeCompany);
